231597d26774bd8e31009483a3af134b1407dd71
ray <rizkyray01@gmail.com>
2024-09-21 14:53:41 +0800

    modify: video & profile section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('hero')
     @include('frontend.hero-section')
     <div class="p-10">
-        @include('frontend.pokja-section')
         @include('frontend.video-section')
+        @include('frontend.pokja-section')
         @include('frontend.profile-section')
         @include('frontend.berita-section')
     </div>

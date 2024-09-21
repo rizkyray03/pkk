231771443b480cd52d53abc0a65fd9df0a589b95
ray <rizkyray01@gmail.com>
2024-09-21 10:57:08 +0800

    first commit

---
+++ resources/views/frontend/video-section.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="flex w-full flex-col">
+        <div class="divider divider-neutral text-3xl font-bold text-info">Video</div>
+    </div>
+
+    <div>
+        <iframe src="https://drive.google.com/file/d/1FNt92TXgSIyKtEkYoiM7Pe4-R_QZj8Kn/preview" width="640"
+            height="480" allow="autoplay" class="responsive-iframe w-full"></iframe>
+
+    </div>
+
+</div>
